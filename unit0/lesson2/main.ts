5f31415d94aaccea2689479b3502985b4cb4741e
Yakov Fain <yakovfain@gmail.com>
2018-07-11 07:59:15 -0400

    added TOC

---
+++ unit0/lesson2/main.ts
@@ -0,0 +1,6 @@
+function getFinalPrice(price: number, discount: number) {
+  return price - price/discount;
+}
+
+console.log(getFinalPrice(100, 10));    
+console.log(getFinalPrice(100, "10%"));
---
+++ unit0/lesson3/dist/main.js
@@ -0,0 +1,5 @@
+function getFinalPrice(price, discount) {
+    return price - price / discount;
+}
+console.log(getFinalPrice(100, 10));
+// console.log(getFinalPrice(100, "10%"));
