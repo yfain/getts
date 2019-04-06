9ed050ecfb6385bb49d584c4eae77f06c08a88bb
Anton Moiseev <a@antonmoiseev.com>
2019-04-06 16:39:43 +0400

    Improve ch7 example

--- chapter7/jquery-ui-example/index.ts
+++ chapter7/jquery-ui-example/index.ts
@@ -1,17 +1,33 @@
 const shapesElement = $('#shapes');
+const errorElement = $('#error');
+const infoElement = $('#info');
 
 $('#selector').autocomplete({
-    source: (request, response) => {
-      try {  
+    source: (request: { term: string }, response: ([]) => void) => {
+      try {
         const elements = $(request.term, shapesElement);
         const ids = elements.map((_index, dom) => ({ label: $(dom).attr('id'), value: request.term })).toArray();
         response(ids);
+        
+        infoElement.text(`Found ${elements.length} element(s)`);
+        errorElement.text('');
       } catch (e) {
-        // Invalid selector
+        response([]);
+        infoElement.text('');
+        errorElement.text('Invalid selector');
+        $('*', shapesElement).css({ border: 'none' });
       }
     },
     focus: (_event, ui) => {
       $('*', shapesElement).css({ border: 'none' });
       $(`#${ui.item.label}`, shapesElement).css({ border: '5px solid red' });
     }
+});
+
+$('#selector').on('input', (event: JQuery.TriggeredEvent<HTMLInputElement>) => {
+  if (!event.target.value) {
+    $('*', shapesElement).css({ border: 'none' });
+    errorElement.text('');
+    infoElement.text('');
+  }
 });
